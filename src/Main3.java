28d2f2b43f0767019314de675974c8873e3be418
luqi2000 <luqmanmohammad09@gmail.com>
2023-05-02 20:13:37 +0100

    aggiunta del perimetro del rettangolo

--- src/Main3.java
+++ src/Main3.java
@@ -1,9 +1,19 @@
-
+import java.util.Scanner;
 public class Main3 {
 
 	public static void main(String[] args) {
-	
-
+		perimetroRettangolo();
 	}
+	public static void perimetroRettangolo() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Insert late number of rectang");
+		int late1 = in.nextInt();
+		System.out.println("Insert second late number of rectang");
+		int late2 = in.nextInt();
+		int result = (late1+late2)*2;
 
+		System.out.println("Il perimetro del rettangolo e' " + result);
+		in.close();
+		
+	}
 }
